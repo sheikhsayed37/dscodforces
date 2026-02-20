8e4a79b9fb54de764698b76146875385a7be09b8
sheikhsayed37 <sheikhmohammedsayedali@gmail.com>
2026-02-20 19:30:35 +0600

    4

--- Y_Range_sum_query.cpp
+++ Y_Range_sum_query.cpp
@@ -10,21 +10,17 @@ int main()
         cin>> a[i];
 
     }
-
     for(int i=0;i<q;i++)
     {
         int l,r;
         cin>>l>>r;
         int sum=0;
-        for(int j=l-1;j<=r-1;j++)
-        {
+        for(int j=l-1;j<=r-1;j++) // 3 no line e 1 3 mane index 0-2 porjonto jog kora 
+        {                 //6+4+2=12 ..akane first line e n=6 jeta array neya hoise '
+                         // or input holo 6 4 2 7 2 7. er pashe 3 holo quaery.
             sum=sum+a[j];
         }
         cout<<sum<<endl;
     }
-    
-
-
-
 
 }
