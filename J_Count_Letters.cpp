d7f97da0f68d9551177bbf4db3995a875ff291f3
sheikhsayed37 <sheikhmohammedsayedali@gmail.com>
2026-02-21 12:09:00 +0600

    6

--- J_Count_Letters.cpp
+++ J_Count_Letters.cpp
@@ -6,20 +6,28 @@ int main()
     string s;
     cin >> s;
 
-    int freq[26] = {0};
+    int freq[26] = {0};// akta array declare korsi dore nisi akne  A-Z porzonto ase
 
     for(int i = 0; i < s.size(); i++)
     {
-        freq[s[i] - 'a']++;
+        freq[s[i] - 'a']++; 
+        //'a' - 'a' = 0
+       //'b' - 'a' = 1
+        //'c' - 'a' = 2
+      //akane frequency namer array te string word gola bosai setake
+        // index e proninoto korsi like  | s[i] | s[i] - 'a' | কি হবে    |
+
+      // a    | 0          | freq[0]++ |
+      //a     | 0          | freq[0]++ |
+      //a     | 0          | freq[0]++ |
+     //b    | 1          | freq[1]++ |
     }
-
-    for(int i = 0; i < 26; i++)
-    {
-
-        if(freq[i] > 0)
+      for(int i = 0; i < 26; i++)
+     {
+         if(freq[i] > 0)
         {
             cout << char(i + 'a') << " : " << freq[i] << endl;
         }
         
-    }
+     }
 }
