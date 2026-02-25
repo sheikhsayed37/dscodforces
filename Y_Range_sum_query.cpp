3a2e2cd12ca217c38660eb29adac6a5c461e7c8e
sheikhsayed37 <sheikhmohammedsayedali@gmail.com>
2026-02-25 23:35:20 +0600

    10

--- Y_Range_sum_query.cpp
+++ Y_Range_sum_query.cpp
@@ -2,16 +2,15 @@
 using namespace std;
 int main()
 {
-    int n,q;
+    long long int n,q;
     cin>>n>>q;
     
-    vector<int>v(n+1);  // akta beshi nite hocce \index jeno 1  teke count korte pari
-    for(int i=1;i<n+1;i++)// oder kotha onosare i=1 to n
+    vector<long long int>v(n+1);    // akta beshi nite hocce \index jeno 1  teke count korte pari
+    for(int i=1;i<n+1;i++)     // oder kotha onosare i=1 to n
     {
         cin>> v[i];
-
     }
-     vector<int>prefix(n+1);
+     vector<long long int>prefix(n+1);
      prefix[1]=v[1];
 
     for(int i=2;i<n+1;i++)
@@ -22,11 +21,10 @@ int main()
     {
         int L,r;
         cin>>L>>r;
-        int sum=0;
-       if(L==1)
+         long long int sum=0;
+       if(L==1)   // jodi L 1 hoy tahole prefix[r] e r porjonto sum ache mane index 1 ortat 6 change hbe na
        {
         sum=prefix[r];
-       
        }
        else
        {
