36b6f21cbb48b940aea20b0679497076e8bb9ea7
sheikhsayed37 <sheikhmohammedsayedali@gmail.com>
2026-02-26 23:44:32 +0600

    12

---
+++ biznrysearch.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    int a[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>a[i];
+    }
+    int value;
+    cin>>value;
+    int flag=0;
+    for(int i=0;i<n;i++)
+    {
+        if(a[i]==value)
+        {
+            flag=1;
+        }
+
+    }
+    if(flag=1)
+    {
+        cout<<"found"<<endl;
+    }
+   else{
+    cout<<"not found"<<endl;
+   }
+
+
+
+
+}
