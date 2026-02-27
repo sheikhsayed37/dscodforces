f2e9f98fc2290f941f9e62f8f65d63c4f1f5b9fd
sheikhsayed37 <sheikhmohammedsayedali@gmail.com>
2026-02-27 18:58:16 +0600

    19

---
+++ G_Summation_from_1_to_N.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h> 
+using namespace std;
+int main()
+{
+  long long int n;
+    cin>>n;
+    long long int sum=n*(n+1)/2;  // 1 theke n porjonto sum er formula holo n*(n+1)/2
+    cout<<sum<<endl;
+}
