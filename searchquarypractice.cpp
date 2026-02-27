2c272e4009e2672a9093ca36c855fdf86e513bca
sheikhsayed37 <sheikhmohammedsayedali@gmail.com>
2026-02-27 16:34:35 +0600

    18

--- searchquarypractice.cpp
+++ searchquarypractice.cpp
@@ -4,6 +4,54 @@ int main()
 {
     int n;
     cin>>n;
-    vector<int>v(n);
+    int a[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>a[i];
+    }
+    sort(a,a+n);
+ 
+    int q;
+    cin>>q;
+   for(int i=0;i<q;i++)
+    {
+    int l,r;
+    l=0;
+    r=n-1;
+    int flag=0;
+       
+        int value;
+        cin>>value;
+        while(l<=r)
+        {
+            int mid=(l+r)/2;
+            if(a[mid]==value)
+            {
+                flag=1;
+                break;
+            }
+            else if(a[mid]>value)
+            {
+               r=mid-1;
+            }
+            else
+            {
+                l=mid+1;
+            }
+        }
+        if(flag==1)
+        {
+            cout<<"YES"<<endl;
+        }
+        else
+        {
+            cout<<"NO"<<endl;
+        }
     
+
+    }
+    
+
+
+
 }
